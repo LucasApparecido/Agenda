c2a141e466f02d42b3e0168fa2e13f7b223213f3
Lucas Apparecido de Oliveira <lucasapparecido91@gmail.com>
2023-06-29 17:13:43 -0300

    tentativa de configurar o utf8

--- Principal.java
+++ Principal.java
@@ -5,6 +5,9 @@
 public class Principal {
 
 	public static void main(String[] args) {
+		// Configuração da codificação UTF-8 para a JVM
+		System.setProperty("file.encoding", "UTF-8");
+		System.setProperty("sun.jnu.encoding", "UTF-8");
 		//inicia o programa
 		TelaPrincipal tela = new TelaPrincipal();
 
